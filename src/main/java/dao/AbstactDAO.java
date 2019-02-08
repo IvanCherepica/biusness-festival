c1164923e297b3ab3c89da34eacb3efb922152f2
morze <amorozov@list.ru>
2019-02-08 15:18:04 +0300

    DAO changed

--- src/main/java/dao/AbstactDAO.java
+++ src/main/java/dao/AbstactDAO.java
@@ -1,23 +1,22 @@
 package dao;
 
-abstract class AbstractDaoImpl<T> implements AbstractDao<T>{
+abstract class AbstactDAO<T> {
 
-    @Override
     public T getById(long id) {
         return null;
     }
 
-    @Override
+
     public void save(T t) {
 
     }
 
-    @Override
+
     public void remove(long id) {
 
     }
 
-    @Override
+
     public void update(T t) {
 
     }
---
+++ src/main/java/dao/HotPointDAOImpl.java
@@ -0,0 +1,25 @@
+package dao;
+
+import models.HotPoint;
+
+public class HotPointDAOImpl implements HotPointDAO {
+    @Override
+    public HotPoint getById(long id) {
+        return null;
+    }
+
+    @Override
+    public void save(HotPoint hotPoint) {
+
+    }
+
+    @Override
+    public void remove(long id) {
+
+    }
+
+    @Override
+    public void update(HotPoint t) {
+
+    }
+}
