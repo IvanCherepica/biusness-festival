47feaac62bd8f50dd239e5cf42ac43f3e425cabb
morze <amorozov@list.ru>
2019-02-08 13:01:06 +0300

    added models MapUnit, PointUnit, Festival, HotPoint, EventPoint

---
+++ src/main/java/models/EventPoint.java
@@ -0,0 +1,10 @@
+package models;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "eventPoints")
+public class EventPoint extends PointUnit {
+
+}
---
+++ src/main/java/models/Festival.java
@@ -0,0 +1,5 @@
+package models;
+
+public class Festival extends MapUnit{
+
+}
