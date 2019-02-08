9a81d27a31f7c186566d92fbcdac68743dabaf70
Matvey Kukharchuk <bull3003@gmail.com>
2019-02-08 17:21:36 +0300

    Adding map, geolocation, geometry of a fastival

---
+++ src/main/java/src/main/java/servlets/FestivalMapServlet.java
@@ -0,0 +1,24 @@
+package src.main.java.servlets;
+
+import com.google.gson.Gson;
+import models.User;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/map")
+public class FestivalMapServlet extends HttpServlet {
+
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        //request.setAttribute("data", geometryJSON);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/map.html");
+        dispatcher.forward(request, response);
+    }
+}
