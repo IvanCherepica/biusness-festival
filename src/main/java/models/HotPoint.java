02caa99f582afa40dc9dc1f78bed14b20add03e8
morze <amorozov@list.ru>
2019-02-09 01:05:21 +0300

    HotPoint, EventPoint update

--- src/main/java/models/HotPoint.java
+++ src/main/java/models/HotPoint.java
@@ -7,4 +7,11 @@ import javax.persistence.Table;
 @Table(name = "hot_point")
 public class HotPoint extends PointUnit{
 
+    public HotPoint() {
+    }
+
+    public HotPoint(String name, String description, String geometry, String color, Festival festival) {
+        super(name, description, geometry, color, festival);
+    }
+
 }
