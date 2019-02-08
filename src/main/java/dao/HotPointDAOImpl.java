89d3cf7aba49e47e175e531ddd158799b11de92d
morze <amorozov@list.ru>
2019-02-08 15:46:41 +0300

    create HotPointDAO

--- src/main/java/dao/HotPointDAOImpl.java
+++ src/main/java/dao/HotPointDAOImpl.java
@@ -1,25 +1,33 @@
 package dao;
 
 import models.HotPoint;
+import org.hibernate.Session;
 
 public class HotPointDAOImpl implements HotPointDAO {
-    @Override
-    public HotPoint getById(long id) {
-        return null;
+    private Session session;
+
+    public HotPointDAOImpl(Session session) {
+        this.session = session;
     }
 
     @Override
-    public void save(HotPoint hotPoint) {
+    public HotPoint getById(long id) {
+        return (HotPoint) session.get(HotPoint.class,id);
+    }
 
+    @Override
+    public long save(HotPoint hotPoint) {
+        return (long) session.save(hotPoint);
     }
 
     @Override
     public void remove(long id) {
-
+        Object persistentInstance = session.load(HotPoint.class,id);
+        session.delete(persistentInstance);
     }
 
     @Override
-    public void update(HotPoint t) {
-
+    public void update(HotPoint hotPoint) {
+        session.update(hotPoint);
     }
 }
