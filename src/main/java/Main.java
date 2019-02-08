753198f88cbdb44da4ba9cb37bbe750fa0f2bcaa
morze <amorozov@list.ru>
2019-02-08 22:05:55 +0300

    Create AbstractDao4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,12 @@
+import models.Festival;
 import services.FestivalServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
         FestivalServiceImpl festivalService = FestivalServiceImpl.getInstance();
 
+
+        festivalService.add(new Festival("festival","festival1","festival2","festival3"));
 
     }
 }
