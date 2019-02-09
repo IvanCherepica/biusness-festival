23d715326c7969edab3c0af9061e3378e8758407
morze <amorozov@list.ru>
2019-02-10 01:49:42 +0300

    UserNotification.js updated 3 web socket began to work

--- src/main/java/servlets/CompaerLocationsServlet.java
+++ src/main/java/servlets/CompaerLocationsServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/compareLocations")
+@WebServlet("/compareLocations11")
 public class CompaerLocationsServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
---
+++ src/main/java/servlets/LocationWebSocketServlet.java
@@ -0,0 +1,40 @@
+package servlets;
+
+import javax.websocket.*;
+import javax.websocket.server.ServerEndpoint;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+@ServerEndpoint(value = "/compareLocations")
+public class LocationWebSocketServlet {
+        private Set<Session> userSessions = Collections.synchronizedSet(new HashSet<Session>());
+
+
+        @OnOpen
+        public void start(Session userSession) {
+            userSessions.add(userSession);
+        }
+
+        @OnClose
+        public void onClose(Session userSession) {
+            userSessions.remove(userSession);
+        }
+
+        @OnMessage
+        public void onMessage(String message, Session userSession) {
+            System.out.println("Message Received: " + message);
+            for (Session session : userSessions) {
+                System.out.println("Sending to " + session.getId());
+                session.getAsyncRemote().sendText(message);
+            }
+        }
+
+        @OnError
+        public void onError(Throwable t) throws Throwable {
+            //TODO
+        }
+
+
+
+}
