927a5f9f144fc73aed8c8c07faee2e0e36b6f38f
Matvey Kukharchuk <bull3003@gmail.com>
2019-02-15 20:20:02 +0300

    Убрал информацию о юзере

--- src/main/webapp/userMap/userNotification.js
+++ src/main/webapp/userMap/userNotification.js
@@ -136,18 +136,21 @@ function processDataForFestivalBlock(event, eventspoints) {
     if(event!==undefined && eventspoints!==undefined) {
         isInFestival = JSON.parse(event.data).isInFestival;
         festival = JSON.parse(event.data).festival;
+        $(".informUser").css('width', '300');
         $("#festivalInfo1").text("You are at " + festival.name);
         $("#festivalInfo2").text("About: " + festival.description);
-        $("#informUserBlock").css('height', '770');
-        $("#festivalBlock").css('height', '450');
+        // $("#festivalBlock").css('height', '450');
         if (flag === false) {
             $("#events").append("<h4><b>Todays events</b></h4>");
             for (var i = 0; i < eventspoints.length; i++) {
                 var j = i + 1;
-                $("#events").append("<li style='margin: 3px 3px 3px 3px;'><a href=\"#\"><button style='color: #fff; /* цвет текста */\n" +
+                $("#events").append("<li style='margin: 3px 3px 3px 3px;border-radius: 20px;'><a href=\"#\"><button style='color: #fff; /* цвет текста */\n" +
                     "  text-decoration: none; /* убирать подчёркивание у ссылок */\n" +
                     "  user-select: none; /* убирать выделение текста */\n" +
                     "  background: rgb(212,75,56); /* фон кнопки */\n" +
+                    "  border-radius: 20px; /* округление */\n" +
+                    "  height: 6% ; /* высота */\n" +
+                    "  width: 100%; /* длина */\n" +
                     "  padding: .1em 0.5em; /* отступ от текста */\n" +
                     "  outline: none; /* убирать контур в Mozilla */' data-toggle=\"collapse\" data-target=\"#demo" + j
                     + "\">"+ eventspoints[i].name +"</button></li></a>\n" +
